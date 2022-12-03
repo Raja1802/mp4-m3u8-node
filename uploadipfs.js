47786cee179b0847d7e703a8268dd15ba5357123
raja1802 <rajagandla16@gmail.com>
2022-12-03 17:01:38 +0530

    Asas

--- uploadipfs.js
+++ uploadipfs.js
@@ -5,40 +5,26 @@ const fs = require("fs");
 
 const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweGJhNWQzZTU2OTlDRTllZDYwMUZhRkUwYzhiZmI1MzJCYjRFYWI5OTgiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2NTQyNTUzNDk0OCwibmFtZSI6InNkc2QifQ.3wabmCtAPSt4_6vNdD0NCLMeZIHvMthxfs9gETb5mq4";
-const directoryPath = process.argv[2];
-
-const storage = new NFTStorage({ token });
-
-async function upload(folder) {
-  console.log(folder);
-  console.log("uploading...");
-  const files = filesFromPath(folder, {
-    pathPrefix: path.resolve(folder),
-    hidden: true,
-  });
-  const cid = await storage.storeDirectory(files);
-  console.log("storef");
-  console.log({ cid });
-  const status = await storage.status(cid);
-  // await sleep(20000);
-  console.log(status);
-}
 
 async function main() {
-  console.log("triggred");
   // you'll probably want more sophisticated argument parsing in a real app
   if (process.argv.length !== 3) {
     console.error(
       `usage: ${process.argv[0]} ${process.argv[1]} <directory-path>`
     );
   }
+  const storage = new NFTStorage({ token });
+  const directoryPath = process.argv[2];
+  const files = filesFromPath(directoryPath, {
+    pathPrefix: path.resolve(directoryPath), // see the note about pathPrefix below
+    hidden: true, // use the default of false if you want to ignore files that start with '.'
+  });
 
-  // console.log(`storing file(s) from ${path}`);
+  console.log(`storing file(s) from ${path}`);
+  const cid = await storage.storeDirectory(files);
+  console.log({ cid });
 
-  for await (const f of filesFromPath(directoryPath)) {
-    console.log(f);
-    await upload(directoryPath);
-  }
-  // main();
+  const status = await storage.status(cid);
+  console.log(status);
 }
 main();
